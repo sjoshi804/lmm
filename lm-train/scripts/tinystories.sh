22d0d5b5e685f26b4fa335327a91b7ede1617a4b
Siddharth Joshi <sjoshi804@gmail.com>
2024-10-18 12:10:04 -0700

    do save scripts

---
+++ lm-train/scripts/tinystories.sh
@@ -0,0 +1,24 @@
+WD="/home/sjoshi/lmm/lm-train"
+
+export WANDB_PROJECT="gpt2-pretrain"
+deepspeed \
+    --include localhost:3,4,5,6,7 \
+    --master_port 29501 \
+    $WD/pretrain_gpt2.py \
+    --deepspeed $WD/ds_configs/zero3.json \
+    --vocab_size 50257 \
+    --n_positions 1024 \
+    --n_embd 768 \
+    --n_layer 12 \
+    --n_head 12 \
+    --intermediate_size 3072 \
+    --dataset_name TinyStories \
+    --max_length 1024 \
+    --output_dir $WD/checkpoints/gpt2-pretrained \
+    --per_device_train_batch_size 8 \
+    --num_train_epochs 1 \
+    --learning_rate 5e-5 \
+    --logging_dir $WD/logs \
+    --logging_steps 10 \
+    --save_strategy epoch \
+    --save_total_limit 2
