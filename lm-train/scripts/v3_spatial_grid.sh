9dc45beb3625e83da53cc3bbe0547b9fd4bb42fd
Siddharth Joshi <sjoshi804@gmail.com>
2024-10-20 10:39:49 -0700

    more scripts

--- lm-train/scripts/v3_spatial_grid.sh
+++ lm-train/scripts/v3_spatial_grid.sh
@@ -1,8 +1,8 @@
 WD="/home/sjoshi/lmm/lm-train"
-RUN_ID=v1_spatial_grid
+RUN_ID=v3_spatial_grid
 export WANDB_PROJECT="gpt2-pretrain"
 deepspeed \
-    --include localhost:0,1,2,3,4,5,6,7 \
+    --include localhost:4,5,6,7 \
     --master_port 29501 \
     $WD/pretrain_gpt2.py \
     --deepspeed $WD/ds_configs/zero3.json \
@@ -12,15 +12,15 @@ deepspeed \
     --n_layer 12 \
     --n_head 12 \
     --intermediate_size 3072 \
-    --dataset_name /home/sjoshi/lmm/data/generated/v1_spatial_grid \
+    --dataset_name /home/sjoshi/lmm/data/generated/v3_spatial_grid \
     --load_from_disk True \
     --split train \
     --max_length 1024 \
     --output_dir $WD/checkpoints/$RUN_ID \
     --per_device_train_batch_size 8 \
-    --num_train_epochs 1 \
+    --num_train_epochs 2 \
     --learning_rate 5e-5 \
     --logging_dir $WD/logs \
     --logging_steps 100 \
     --save_strategy epoch \
-    --save_total_limit 2
+    --save_total_limit 1
