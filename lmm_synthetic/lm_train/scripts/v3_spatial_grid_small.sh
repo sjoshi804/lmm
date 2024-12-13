1d1706f7e1837b0b4be2797fd2d6bf53d212bb93
Siddharth Joshi <sjoshi804@gmail.com>
2024-12-12 16:35:15 -0800

    small data script

--- lmm_synthetic/lm_train/scripts/v3_spatial_grid_small.sh
+++ lmm_synthetic/lm_train/scripts/v3_spatial_grid_small.sh
@@ -2,7 +2,7 @@ WD="/home/sjoshi/lmm/lmm_synthetic/lm_train"
 RUN_ID=v3_spatial_grid_small
 export WANDB_PROJECT="lm_train"
 deepspeed \
-    --include localhost:0,1,2,3,4,5,6,7 \
+    --include localhost:0,1,2,3 \
     --master_port 29501 \
     $WD/pretrain_lm.py \
     --deepspeed $WD/ds_configs/zero3.json \
@@ -12,19 +12,19 @@ deepspeed \
     --n_layer 8 \
     --n_head 8 \
     --intermediate_size 2048 \
-    --dataset_name /home/sjoshi/lmm/data/generated/v3_spatial_grid \
+    --dataset_name /home/sjoshi/lmm/lmm_synthetic/data/generated/v3_spatial_grid \
     --load_from_disk True \
     --split train \
     --max_length 128 \
     --output_dir $WD/checkpoints/$RUN_ID \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 64 \
     --num_train_epochs 10 \
     --learning_rate 2e-3 \
     --weight_decay 0.05 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 2 \
     --warmup_steps 100 \
     --lr_scheduler_type cosine \
     --logging_dir $WD/logs \
-    --logging_steps 100 \
+    --logging_steps 10 \
     --save_strategy epoch \
     --save_total_limit 1
